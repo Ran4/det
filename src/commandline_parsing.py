1423414d07cd37a37b83edb941021cf05f420056
Rasmus Ansin <rasmus.ansin@gmail.com>
2018-05-17 13:54:29 +0200

    Enable parsing of --descriptors-file and --descriptor-aliases-file

--- src/commandline_parsing.py
+++ src/commandline_parsing.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import argparse
 
+def add_shared_args_to_parser(parser) -> None:
+    # We're not using type=file since that leaves a dangling file object
+    # and that's just... sloppy
+    parser.add_argument("--descriptors-file", default=None)
+    parser.add_argument("--descriptor-aliases-file", default=None)
+
 def add_reading_args_to_parser(parser) -> None:
     """Used by the GET and STREAM subparsers
     """
@@ -19,6 +25,7 @@ def add_reading_args_to_parser(parser) -> None:
 def add_get_subcommand_parser(subparsers) -> None:
     get_subparser = subparsers.add_parser("get", help="Get objects")
     add_reading_args_to_parser(get_subparser)
+    add_shared_args_to_parser(get_subparser)
     
     get_subparser.set_defaults(random=True)
     get_subparser.set_defaults(subcommand="get")
@@ -27,6 +34,7 @@ def add_stream_subcommand_parser(subparsers) -> None:
     stream_subparser = subparsers.add_parser("stream",
         help="Get objects as a stream")
     add_reading_args_to_parser(stream_subparser)
+    add_shared_args_to_parser(stream_subparser)
     stream_subparser.set_defaults(subcommand="stream")
     
 def get_toplevel_parser() -> argparse.ArgumentParser:
--- src/descriptor_config.py
+++ src/descriptor_config.py
@@ -5,13 +5,14 @@ import os
 
 from setup_logging import log
 
+opj = os.path.join
+
 class DescriptorConfig:
     def __init__(self, descriptors, descriptor_aliases):
         self.available_descriptors: Dict = descriptors
         self.descriptor_aliases: Dict = descriptor_aliases
         
 def try_get_descriptors_filename() -> str:
-    opj = os.path.join
     possible_paths: List[str] = [
         opj("default_descriptor_config", "descriptors.json"),
         opj(os.environ["HOME"], ".config", "det", "descriptors.json"),
@@ -27,16 +28,33 @@ def try_get_descriptors_filename() -> str:
         
     log.error("Couldn't find descriptors file anywhere")
     raise FileNotFoundError("Could not find descriptors.json")
+
+def try_get_descriptor_aliases_filename() -> str:
+    possible_paths: List[str] = [
+        opj("default_descriptor_config", "descriptor_aliases.json"),
+        opj(os.environ["HOME"], ".config", "det", "descriptor_aliases.json"),
+        "descriptor_aliases.json",
+        opj("default_descriptor_config", "descriptor_aliases.json"),
+    ]
+    
+    for possible_path in possible_paths:
+        log.debug("Looking for descriptor aliases file in `%s`", possible_path)
+        if os.path.isfile(possible_path):
+            log.debug("Found descriptor aliases file `%s`", possible_path)
+            return possible_path
+        
+    log.error("Couldn't find descriptor aliases file anywhere")
+    raise FileNotFoundError("Could not find descriptor_aliases.json")
         
 def descriptor_config_from_args(args) -> DescriptorConfig:
-    descriptors_filename = try_get_descriptors_filename()
-        
+    descriptors_filename = \
+        args.descriptors_file or try_get_descriptors_filename()
     with open(descriptors_filename) as f:
         data = f.read()
         descriptors = json.loads(data)
         
-    aliases_filename = os.path.join("default_descriptor_config",
-                                    "descriptor_aliases.json")
+    aliases_filename = \
+        args.descriptor_aliases_file or try_get_descriptor_aliases_filename()
     with open(aliases_filename) as f:
         data = f.read()
         descriptor_aliases = json.loads(data)
